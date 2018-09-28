af10b7dce67316e242c05b7cdc22d780d1536bca
naito-eiichi <41862046+naito-eiichi@users.noreply.github.com>
2018-09-28 21:12:03 +0900

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+typedef struct {
+    void *data;
+    void *prev;
+}StackCell;
+
+typedef struct stack {
+    StackCell *sp;
+
+    size_t data_size;
+    size_t stack_size;
+
+    void (*init)(struct stack *, const size_t);
+    int (*push)(struct stack *, const void *);
+    int (*pop)(struct stack *, void *);
+    size_t (*size)(struct stack *);
+    void (*clear)(struct stack *);
+} Stack;
+
+void initStack(struct stack *s, const size_t data_size);
+int Stack_push(struct stack *s, const void *data);
+int Stack_pop(struct stack *s, void *data);
+size_t Stack_size(struct stack *s);
+void Stack_clear(struct stack *s);

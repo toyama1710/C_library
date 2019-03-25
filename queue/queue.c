32557845bd806ba1d6ce90e9a20e20804c9dfceb
naito-eiichi <walker2002226@gmail.com>
2019-03-25 17:18:44 +0900

    構造体と操作関数を決定

--- queue/queue.c
+++ queue/queue.c
@@ -1,90 +1,81 @@
 #include "queue.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 void initQueue(struct queue *q, const size_t data_size)
 {
-    q->current = NULL;
-    q->last = NULL;
+	q->current = NULL;
+	q->last = NULL;
 
-    q->data_size = data_size;
-    q->que_size = 0;
+	q->data_size = data_size;
+	q->que_size = 0;
 
-    q->init = initQueue;
-    q->size = Que_size;
-    q->enque = Que_enque;
-    q->deque = Que_deque;
-    q->clear = Que_clear;
-
-    return;
+	return;
 }
 
 int Que_enque(struct queue *q, const void *data)
 {
-    QueCell *new_data = (QueCell *)malloc(sizeof(QueCell));
+	QueCell *new_data = (QueCell *)malloc(sizeof(QueCell));
 
-    if (new_data == NULL) {
-        return -1;
-    }
+	if (new_data == NULL) {
+		return -1;
+	}
 
-    new_data->data = malloc(q->data_size);
-    new_data->next = NULL;
+	new_data->data = malloc(q->data_size);
+	new_data->next = NULL;
 
-    if (new_data->data == NULL) {
-        return -1;
-    } else {
-        memcpy(new_data->data, data, q->data_size);
-        ++(q->que_size);
-    }
-    
-    if (q->que_size <= 1) {
-        q->current = new_data;
-        q->last = new_data;
-    } else {
-        q->last->next = new_data;
-        q->last = new_data;
-    }
-    
+	if (new_data->data == NULL) {
+		return -1;
+	} else {
+		memcpy(new_data->data, data, q->data_size);
+		++(q->que_size);
+	}
 
-    return 0;
- }
+	if (q->que_size <= 1) {
+		q->current = new_data;
+		q->last = new_data;
+	} else {
+		q->last->next = new_data;
+		q->last = new_data;
+	}
+
+
+	return 0;
+}
 
 int Que_deque(struct queue *q, void *data)
 {
-    if (q->que_size > 0) {
-        QueCell *next = q->current->next;
+	if (q->que_size > 0) {
+		QueCell *next = q->current->next;
 
-        --(q->que_size);
-        memcpy(data, q->current->data, q->data_size);
+		--(q->que_size);
+		memcpy(data, q->current->data, q->data_size);
 
-        free(q->current->data);
-        free(q->current);
-        q->current = next;
-    } else {
-        return -1;
-    }
+		free(q->current->data);
+		free(q->current);
+		q->current = next;
+	} else {
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
 size_t Que_size(struct queue *q)
 {
-    return q->que_size;
+	return q->que_size;
 }
 
 void Que_clear(struct queue *q)
 {
-    while (q->current != NULL) {
-        QueCell *next = q->current->next;
+	while (q->current != NULL) {
+		QueCell *next = q->current->next;
 
-        free(q->current->data);
-        free(q->current);
+		free(q->current->data);
+		free(q->current);
 
-        q->current = next;
-    }
+		q->current = next;
+	}
 
-    initQueue(q, q->data_size);
+	initQueue(q, q->data_size);
 
-    return;
+	return;
 }
--- queue/queue.h
+++ queue/queue.h
@@ -1,31 +1,56 @@
-#ifndef __Ei1710_Queue_H
-#define __Ei1710_Queue_H
+#ifndef Ei1710_Queue_H
+#define Ei1710_Queue_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+//線形リストでQueueを実装する
+
+//リストのセル構造体
+//data:実データへのポインタ, next:次セル
 typedef struct que_cell{
-    void *data;
-    struct que_cell *next;
+    void *data; 
+    struct que_cell *next; 
 } QueCell;
 
+//head:次にデータを取り出す位置
+//last:次にデータを格納する位置 (の一つ前)
+//data_size:格納データの大きさ
+//que_length:格納データの個数 = キューの大きさ
 typedef struct queue {
-    QueCell *current;
+    QueCell *head;
     QueCell *last;
-    
-    size_t data_size;
-    size_t que_size;
 
-    void (*init)(struct queue *, const size_t);
-    int (*enque)(struct queue *, const void *);
-    int (*deque)(struct queue *, void *);
-    size_t (*size)(struct queue *);
-    void (*clear)(struct queue *);
+    size_t data_size;
+    size_t que_length;
 } Queue;
 
-void initQueue(struct queue *q, const size_t data_size);
-int Que_enque(struct queue *q, const void *data);
-int Que_deque(struct queue *q, void *data);
-size_t Que_size(struct queue *q);
-void Que_clear(struct queue *q);
+//Queue構造体の初期化を行う
+//q:初期化するQueue構造体へのポインタ
+//data_size:格納するデータの大きさ
+//Queueを使う前に必ず呼び出すこと！
+// ex) int型を格納する場合) Queue_init(&que, sizeof(int));
+void Queue_init(struct queue *q, const size_t data_size);
+
+//enque
+//q:操作対象のQueueへのポインタ
+//第二引数:格納するデータ(値渡し)
+//正しく格納できれば正数 エラーが起きたとき0を返す
+int Queue_enque(struct queue *q, ...);
+
+//deque
+//q:操作対象のQueueへのポインタ
+//data:取り出したデータの格納場所
+//キューが空(エラー)のとき0, 正常に取り出したとき正数を返す
+//dataが指す領域がデータを格納するのに足らない場合の動作は不定である
+int Queue_deque(struct queue *q, void *data);
+
+//キューに格納されているデータ数を返す
+size_t Queue_size(struct queue *q);
+
+//Queueをクリアする
+//Queueを使い終わったら必ず呼び出すこと！
+void Queue_clear(struct queue *q);
 
 #endif

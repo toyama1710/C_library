af6a29d47266796adb5f88853bbc5a4d4f38109a
naito-eiichi <walker2002226@gmail.com>
2019-03-25 19:26:42 +0900

    vmemcpy()は整数拡張のために正しく動作しないので削除

--- utility/utility.c
+++ utility/utility.c
@@ -1,19 +1,4 @@
-//引数リストから1byteずつcpにコピー
-void *vmemcpy(void *cp, va_list org, size_t size) 
-{
-	va_list dat;
-	unsigned char byte;
-
-	//org を dat コピー, orgへの副作用を無くす
-	va_copy(dat, org);
-
-	while(size--) {
-		byte = va_arg(dat, unsigned char);
-		cp[size] = byte;
-	}
-
-	return cp;
-}
+#include "utility.h"
 
 int finput(FILE *source, const char *format, ...)
 {
--- utility/utility.h
+++ utility/utility.h
@@ -2,9 +2,5 @@
 #include <stdarg.h>
 #include <string.h>
 
-//引数リストorgが持つデータをcpにsizeバイト分コピー
-//おおよそ memcpy() と同じ
-void *vmemcpy(void *cp, va_list org, size_t size); 
-
 //fscanf()と同じ機能 ただし、変換に失敗した文字がバッファに残らない
 int finput(FILE *source, const char *format, ...);

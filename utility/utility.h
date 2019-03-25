8a55100bf30d674a40f08d0c3ce57fa6def13f19
naito-eiichi <walker2002226@gmail.com>
2019-03-25 23:34:27 +0900

    インクルードガード

--- utility/utility.h
+++ utility/utility.h
@@ -1,6 +1,11 @@
+#ifndef EI1710_UTILITY_H
+#define EI1710_UTILITY_H
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 
 //fscanf()と同じ機能 ただし、変換に失敗した文字がバッファに残らない
 int finput(FILE *source, const char *format, ...);
+
+#endif

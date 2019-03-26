edd8a0cbff3c62c3b87f29fec356fb93b43d4ece
naito-eiichi <walker2002226@gmail.com>
2019-03-26 21:51:28 +0900

    sort()

--- utility/utility.h
+++ utility/utility.h
@@ -5,7 +5,14 @@
 #include <stdarg.h>
 #include <string.h>
 
+#define max(X, Y) ((X) > (Y) ? (X) : (Y))
+#define min(X, Y) ((X) < (Y) ? (X) : (Y))
+
 //fscanf()と同じ機能 ただし、変換に失敗した文字がバッファに残らない
 int finput(FILE *source, const char *format, ...);
+
+//O(n log n) のソート
+int sort(void *base, size_t n_memb, size_t data_size, 
+		int (*cmp)(const void *, const void *));
 
 #endif

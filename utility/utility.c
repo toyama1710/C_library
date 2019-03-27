566a37f37fbbf7811c6beb2d5668bbba92eadb01
Ei1710 <41862046+naito-eiichi@users.noreply.github.com>
2019-03-27 12:23:12 +0900

    Add files via upload

--- utility/utility.c
+++ utility/utility.c
@@ -14,3 +14,70 @@ int finput(FILE *source, const char *format, ...)
 
 	return result;
 }
+
+//”äŠrŠÖ”ŒQ
+int int_compare(int *x, int *y)
+{
+	if (*x > *y) {
+		return 1;
+	} else if (*x == *y) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+int llong_compare(long long *x, long long *y)
+{
+	if (*x > *y) {
+		return 1;
+	} else if (*x == *y) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+int dbl_compare(double *x, double *y)
+{
+	if (*x > *y) {
+		return 1;
+	} else if (*x < *y) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+int int_greater_compare(int *x, int *y)
+{
+	if (*x > *y) {
+		return -1;
+	} else if (*x == *y) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int llong_greater_compare(long long *x, long long *y)
+{
+	if (*x > *y) {
+		return -1;
+	} else if (*x == *y) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int dbl_greater_compare(double *x, double *y)
+{
+	if (*x > *y) {
+		return -1;
+	} else if (*x < *y) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
--- utility/utility.h
+++ utility/utility.h
@@ -15,4 +15,13 @@ int finput(FILE *source, const char *format, ...);
 int sort(void *base, size_t n_memb, size_t data_size, 
 		int (*cmp)(const void *, const void *));
 
+//î‰ärä÷êîåQ
+int int_compare(int *x, int *y);
+int llong_compare(long long *x, long long *y);
+int dbl_compare(double *x, double *y);
+
+int int_greater_compare(int *x, int *y);
+int llong_greater_compare(long long *x, long long *y);
+int dbl_greater_compare(double *x, double *y);
+
 #endif

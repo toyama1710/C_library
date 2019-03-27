223503258de481c611da1ebfd3d4e97a71418a52
Ei1710 <41862046+naito-eiichi@users.noreply.github.com>
2019-03-27 11:14:08 +0900

    Add files via upload

--- vector/vector.c
+++ vector/vector.c
@@ -1,7 +1,7 @@
 #include "vector.h"
 
 //vectorの初期化
-int Vector_init(struct vector *vec, size_t data_size, size_t vec_size)
+int Vector_init(Vector *vec, size_t data_size, size_t vec_size)
 {
 	vec->data_size = data_size;
 	vec->capacity = 0;
@@ -24,7 +24,7 @@ int Vector_init(struct vector *vec, size_t data_size, size_t vec_size)
 }
 
 //vector末尾にデータを追加
-int Vector_push_back(struct vector *vec, void *data)
+int Vector_push_back(Vector *vec, void *data)
 {
 	size_t indx = vec->array_size;
 	size_t new_capacity = max(vec->capacity + 1, vec->capacity * 2);
@@ -55,7 +55,7 @@ int Vector_push_back(struct vector *vec, void *data)
 }
 
 //vectorが管理するデータにアクセス
-void *Vector_array(struct vector *vec, size_t i)
+void *Vector_array(Vector *vec, size_t i)
 {
 	if (i >= vec->array_size || i < 0) {
 		return NULL;
@@ -64,8 +64,20 @@ void *Vector_array(struct vector *vec, size_t i)
 	}
 }
 
+//vector末尾の要素を削除
+int Vector_pop_back(Vector *vec)
+{
+	if (vec->array_size == 0) {
+		return 0;
+	} else {
+		vec->array_size--;
+	}
+
+	return 1;
+}
+
 //vectorをクリア
-void Vector_clear(struct vector *vec)
+void Vector_clear(Vector *vec)
 {
 	free(vec->storage);
 
@@ -75,13 +87,13 @@ void Vector_clear(struct vector *vec)
 }
 
 //vectorに格納しているデータの個数を返す
-size_t Vector_size(struct vector *vec)
+size_t Vector_size(Vector *vec)
 {
 	return vec->array_size;
 }
 
 //キャパシティを返す
-size_t Vector_capacity(struct vector *vec)
+size_t Vector_capacity(Vector *vec)
 {
 	return vec->capacity;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -14,7 +14,7 @@ typedef unsigned char Byte;
 //要素へのアクセスはsizeof(Type) * index バイト分先の要素を見て実現
 //あらかじめ大き目に領域を取っておき、キャパシティオーバーしたらrealloc()する
 
-typedef struct vector {
+typedef struct {
     Byte *storage; //配列領域
 
     size_t data_size; //格納データ型のサイズ
@@ -23,22 +23,25 @@ typedef struct vector {
 } Vector;
 
 //vectorの初期化
-int Vector_init(struct vector *vec, size_t data_size, size_t vec_size);
+int Vector_init(Vector *vec, size_t data_size, size_t vec_size);
 
 //vector末尾にデータを追加
-int Vector_push_back(struct vector *vec, void *data);
+int Vector_push_back(Vector *vec, void *data);
 
 //vectorが管理するデータにアクセス
-void *Vector_array(struct vector *vec, size_t i);
+void *Vector_array(Vector *vec, size_t i);
+
+//末尾のデータを削除
+int Vector_pop_back(Vector *vec);
 
 //vectorをクリア
-void Vector_clear(struct vector *vec);
+void Vector_clear(Vector *vec);
 
 //vectorに格納しているデータの個数を返す
-size_t Vector_size(struct vector *vec);
+size_t Vector_size(Vector *vec);
 
 //キャパシティを返す
-size_t Vector_capacity(struct vector *vec);
+size_t Vector_capacity(Vector *vec);
 
 
 // int

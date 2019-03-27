0a5632e4c032f01a6fec0ffada75a5ee1c3edab1
Ei1710 <41862046+naito-eiichi@users.noreply.github.com>
2019-03-27 12:17:22 +0900

    Add files via upload

--- heap/heap.c
+++ heap/heap.c
@@ -4,8 +4,8 @@
 #define left_index(X) ((X) * 2 + 1)
 #define right_index(X) ((X) * 2 + 2)
 
-void Heap_trickleDown(int i); 
-void Heap_bubble_up(int i);
+void Heap_trickle_down(Heap *heap, int i); 
+void Heap_bubble_up(Heap *heap, int i);
 //size Byteのメモリオブジェクトを入れ替え
 void memswap(void *x, void *y, size_t size);
 
@@ -17,42 +17,93 @@ void Heap_init(Heap *heap, size_t data_size, int (*compare)(const void *, const 
 	return;
 }
 
-int Heap_add(heap *heap, void *data)
+void Heap_top(Heap *heap, void *data)
+{
+	memcpy(data, Vector_array(&(heap->storage), 0), 
+			heap->storage.data_size);
+
+	return;
+}
+
+int Heap_add(Heap *heap, void *data)
 {
 	if (Vector_push_back(&(heap->storage), data) == 0) {
 		return 0;
 	}
 
-	Heap_bubble_up(Vector_size(&(heap->storage)) - 1);
+	Heap_bubble_up(heap, Vector_size(&(heap->storage)) - 1);
 
 	return 1;
 }
 
-void Heap_top(Heap *heap, void *data)
+void Heap_remove(Heap *heap)
 {
-	memcpy(data, Vector_array(&(heap->storage), 0), 
-		heap->storage->data_size);
+	memswap(Vector_array(&(heap->storage), 0),
+			Vector_array(&(heap->storage), Vector_size(&(heap->storage)) - 1),
+			heap->storage.data_size);
+	Vector_pop_back(&(heap->storage));
+
+	Heap_trickle_down(heap, 0);
 
 	return;
 }
 
-void Heap_remove(Heap *heap)
+void Heap_clear(Heap *heap)
 {
+	Vector_clear(&(heap->storage));
 
+	return;
 }
 
 //----
-void Heap_bubble_up(int i) {
-	int p = parent(i);
-	
+void Heap_trickle_down(Heap *heap, int i)
+{
+	do {
+		int j = -1;
+		int r = right_index(i);
+
+		if (r < Vector_size(&(heap->storage)) &&
+				heap->compare(Vector_array(&(heap->storage), r),
+					Vector_array(&(heap->storage), i)) < 0) {
+
+			int l = left_index(i);
+
+			if (heap->compare(Vector_array(&(heap->storage), l),
+						Vector_array(&(heap->storage), r)) < 0) {
+				j = l;
+			} else {
+				j = r;
+			}
+		} else {
+			int l = left_index(i);
+			if (l < Vector_size(&(heap->storage)) &&
+					heap->compare(Vector_array(&(heap->storage), l),
+						Vector_array(&(heap->storage), i)) < 0) {
+				j = l;
+			}
+		}
+
+		if (j >= 0) {
+			memswap(Vector_array(&(heap->storage), i),
+					Vector_array(&(heap->storage), j),
+					heap->storage.data_size);
+		}
+		i = j;
+	} while(i >= 0);
+}
+
+void Heap_bubble_up(Heap *heap, int i)
+{
+	int p = parent_index(i);
+
 	while(i > 0 && heap->compare(Vector_array(&(heap->storage), i),
 				Vector_array(&(heap->storage), p)) < 0) {
 
 		memswap(Vector_array(&(heap->storage), i),
 				Vector_array(&(heap->storage), p),
-				heap->storage->data_size);
+				heap->storage.data_size);
 		i = p;
-		p = parent(i);
+		p = parent_index(i);
 	}
 
 	return;
--- heap/heap.h
+++ heap/heap.h
@@ -9,9 +9,10 @@ typedef struct {
 	int (*compare)(const void *, const void *);
 } Heap;
 
-void Heap_init(Heap *heap, size_t data_size);
+void Heap_init(Heap *heap, size_t data_size, int (*compare)(const void *, const void*));
 int Heap_add(Heap *heap, void *);
 void Heap_top(Heap *heap, void *);
 void Heap_remove(Heap *heap);
+void Heap_clear(Heap *heap);
 
 #endif
